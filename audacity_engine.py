45e95afeaf17e495268d3f94a49cc68eaf6903bf
SaketAnand <saketurfrnd@gmail.com>
2026-06-26 13:55:00 +0530

    Update audacity_engine.py

--- audacity_engine.py
+++ audacity_engine.py
@@ -1,4 +1,15 @@
 class AudacityEngine:
 
     def __init__(self):
-        pass
+        self.to_pipe_name = r'\\.\pipe\ToSrvPipe'
+        self.from_pipe_name = r'\\.\pipe\FromSrvPipe'
+
+        self.to_pipe = None
+        self.from_pipe = None
+    def connect(self):
+    print("Connecting to Audacity...")
+
+    self.to_pipe = open(self.to_pipe_name, "w")
+    self.from_pipe = open(self.from_pipe_name, "r")
+
+    print("Connected!")    
